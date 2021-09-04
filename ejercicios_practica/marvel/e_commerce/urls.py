1f589509eddfa35af8fe6c9cbec050e7ab305459
PatricioHenderson <patricio@avradev.com>
2021-09-04 10:18:30 -0300

    Ejercicios resueltos

--- ejercicios_practica/marvel/e_commerce/urls.py
+++ ejercicios_practica/marvel/e_commerce/urls.py
@@ -7,7 +7,12 @@ from e_commerce.views import *
 
 urlpatterns = [
     # NOTE: e_commerce base:
-    path('base', PruebaView.as_view()),
+    path('base', PruebaView.as_view(), name ='base'),
     
     #TODO: Tarea! 
-    ]
+    path('cart' , CartView.as_view(), name = 'cart'),
+    path('login', LoginView.as_view(), name='login'),
+    path('thanks',ThanksView.as_view(), name = 'thanks'),
+    path('user' , UserView.as_view(),name = 'user') ,
+    path('wish', WishView.as_view(), name = 'wish'),
+    ] 
--- ejercicios_practica/marvel/e_commerce/views.py
+++ ejercicios_practica/marvel/e_commerce/views.py
@@ -4,8 +4,39 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Importamos los modelos que vamos a usar:
-from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import User 
 from e_commerce.models import *
 
 class PruebaView(TemplateView):
-    template_name = 'e-commerce/base.html'
+    template_name = 'e-commerce/base.html'  
+
+class CartView(TemplateView):
+    template_name = 'e-commerce/cart.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['Cart'] = WishList.objects.filter(user_id=self.request.user.id, cart=True)
+        
+        return context
+
+class LoginView(LoginView):
+    template_name = 'e-commerce/login.html'
+
+class ThanksView(TemplateView):
+    template_name = 'e-commerce/thanks.html'
+
+class UserView(TemplateView):
+    template_name = 'e-commerce/user.html'    
+
+class WishView(TemplateView):
+
+    template_name = 'e-commerce/wish.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['WishList'] = WishList.objects.filter(user_id=self.request.user.id, favorite=True)
+        
+        return context
